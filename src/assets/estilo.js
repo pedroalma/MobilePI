03932713f7d71df4d6be4983710ac537f5f4fd29
pedroalma <pedroalma554@gmail.com>
2025-10-17 11:14:35 -0300

    1.0.2

---
+++ src/assets/estilo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet } from "react-native"
+
+export default estilo = StyleSheet.create({
+    txtGrande:{
+        fontSize: 40,
+        fontFamily : "Roboto-Bold",
+    },
+    txtMedio:{
+        fontSize: 30,
+        fontFamily : "Roboto-Bold",
+    },
+    txtPequeno:{
+        fontSize: 18,
+        fontFamily : "Roboto-Bold",
+    }
+});
